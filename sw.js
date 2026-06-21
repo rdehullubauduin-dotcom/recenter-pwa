a81a09f2c3ef826dcc9ec64df6169bd654914df2
rdehullubauduin-dotcom <rdehullubauduin@gmail.com>
2026-06-21 08:09:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,11 @@
-const CACHE_NAME = 'recenter-v2';
-
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-for (let i = 1; i <= 65; i++) {
-  const number = String(i).padStart(3, '0');
-  FILES_TO_CACHE.push(`/pages/${number}PWA.png`);
-}
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request);
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.registration.unregister())
   );
 });
